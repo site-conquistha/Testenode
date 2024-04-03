39bf94ea0c333f81a63c77765f44f18bcb46d73f
site-conquistha <formulariositeconquistha@gmail.com>
2024-04-03 15:56:06 -0300

    mais 1

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const transporter = Nodemailer.createTransport({
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://testenode-nine.vercel.app",
+    origin: "https://testenode-cliente.vercel.app",
   })
 );
 
